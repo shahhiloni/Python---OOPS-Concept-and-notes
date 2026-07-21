bb232f63b73f8e43f71c711658f51ad429304ec2
shahhiloni <shahhiloni411@gmail.com>
2026-07-21 09:38:24 +0530

    hello

---
+++ src/File_Handling.md
@@ -0,0 +1,11 @@
+# file handling in python : in file handling we creating data, manage data, control data, reading, writing, data update, deleting data
+
+# 1. Openinig a file
+open() - function 
+
+# 2. reading a file - read entire data of the file 
+#3. writing a file - write only (creating data in a file )
+# 4. deleting a file - deleting data from the file or delete the entire file 
+# 5. appending a file - update the existing data 
+
+
---
+++ src/File_Handling.py
@@ -0,0 +1,48 @@
+## Opening a file  : open() 
+
+# file = open ("filename.txt", "mode")
+
+
+# mode : 
+# "r" - read
+# "w" - write 
+# "a" - append
+# "x" - creating a new file (fails if the file already exists)
+# "rb" - read binary file 
+# "wb" - write binary file 
+
+# reading a file : read(), readline(), readlines()
+
+file = open ("filename.txt", "r")
+print(file.read())
+file.close()
+
+#writing a file :  write()
+file = open ("filename.txt", "w")
+file.write("hello, Good morning")
+file.close()
+
+## appending a file 
+file = open("filename.txt", "a")
+file.write("\n welcome")
+file.close()
+
+import os 
+
+if os.path.exists("File_Handling.txt"):
+    print("file exists")
+else: 
+    print("file not found")
+
+
+#deleting a file - remove()
+
+import os 
+os.remove("filename.txt")
+
+
+#with statement - using for automatically closing the file 
+
+with open("filename.txt", "w") as file:
+    file.write("hello, Good morning")
+
